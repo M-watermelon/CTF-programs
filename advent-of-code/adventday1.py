d8ed3e6933e4e0ce4deecc20ac99a9479cc57a60
Maria M <87876938+M-watermelon@users.noreply.github.com>
2022-12-01 22:28:14 -0600

    Add files via upload

---
+++ advent-of-code/adventday1.py
@@ -0,0 +1,25 @@
+file = open(r'input.txt', 'r')
+valuesList = []
+sumsList = []
+
+file.readline()
+
+for i in file:
+    input = i
+    if input == "\n":
+        sumsList.append(sum(valuesList))
+        valuesList = []
+    else:
+        input = int(input)
+        valuesList.append(input)
+sumsList.sort()
+
+newlist = []
+
+newlist.append(sumsList[-1])
+newlist.append(sumsList[-2])
+newlist.append(sumsList[-3])
+
+print(sum(newlist))
+
+file.close()
